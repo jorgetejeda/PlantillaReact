3a64e0135db0fe6fbefaa327e83ab82c4c0525e9
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-10-05 23:15:59 -0400

    Se diseno el formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 import "./App.css";
 
 const App = () => (
-  <Container className="text-center">
-    <Button>Thank you, next</Button>
+  <Container className="w-75 m-auto">
+    <Row>
+      <Col>
+        <h1>Informacion</h1>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Nombre del banco</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Nombre candidato</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Cedula</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Nombre posicion</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Salario</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Atentido Por</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Button variant="success" className="mr-1">
+            Exportar Word
+          </Button>
+          <Button variant="danger">Limpiar</Button>
+        </Form>
+      </Col>
+      <Col></Col>
+    </Row>
   </Container>
 );
 
