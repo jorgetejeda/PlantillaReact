cc9e819e3d744c321fd150171fa8bdc000b76cb4
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-10-05 23:25:12 -0400

    Se creo el documento el cual se ira llenando conforme se vayan completando el formulario

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 
 const App = () => (
   <Container className="w-75 m-auto">
-    <Row>
+    <Row className="mt-5">
       <Col>
         <h1>Informacion</h1>
         <Form>
@@ -46,7 +46,66 @@ const App = () => (
           <Button variant="danger">Limpiar</Button>
         </Form>
       </Col>
-      <Col></Col>
+      <Col>
+        <div id="source-html" class="page p-3">
+          <center>
+            <div></div>
+            <span style={{ fontSize: "13px" }}>
+              <b>RNC #-##-#####-#, RNL #-##-######-####</b>
+            </span>
+            <span style={{ fontSize: "11.5px" }}>
+              <div>
+                Plantilla de contratos - Desarrollado con React
+              </div>
+              Teléfonos: ###-###-####
+            </span>
+          </center>
+          <br />
+          <br />
+          <br />
+          <br />
+          <div style={{ fontSize: "14px" }}>
+            6 de Octubre de 2020
+            <br />
+            Santo Domingo, R.D
+          </div>
+          <br />
+          <br />
+          <br />
+          <div style={{ fontSize: "14px" }}>
+            Distinguidos señores del adsadsf:
+          </div>
+          <br />
+          <br />
+          <br />
+          <div>
+            <p style={{ fontSize: "14px" }}>
+              Por medio de la presente nos dirigimos a ustedes para certificar
+              que el(la) señor(a) <b>asasdfas</b> portador(a) de la cédula{" "}
+              <b>No. </b> labora en nuestra empresa en la posición{" "}
+              <b>ASDFASDF</b> desde el <b>6 de Octubre de 2020</b>, devengando
+              un salario por hora de{" "}
+              <b>RD$asdfas PESOS DOMINICANOS CON 00/100</b>, sin sus respectivos
+              incentivos incluidos. Solicitamos la apertura de una cuenta nomina
+              en su banco.
+            </p>
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <div style={{ fontSize: "14px" }}>
+            <span style={{ fontSize: "15px" }}>
+              <b>asdfasd</b>
+            </span>
+            <br />
+            Coordinadora de Reclutamiento
+            <br />
+            Working Bees DR, SRL
+          </div>
+        </div>
+      </Col>
     </Row>
   </Container>
 );
